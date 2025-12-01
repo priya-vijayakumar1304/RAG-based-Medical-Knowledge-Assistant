184079a84fa915b68323d1b073ea8764cbaccfc0
Priya V <priya.vijayakumar1304@gmail.com>
2025-12-01 19:46:16 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="RAG-based Medical Knowledge Assistant",
+    version="0.1.0",
+    author="Priya V",
+    author_email="priya.vijayakumar1304@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
